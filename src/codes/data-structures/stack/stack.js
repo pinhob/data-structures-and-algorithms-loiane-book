2bf97bfd67cf6988650915332d5820fa0f869db8
Bruno Pinho <brunopinho@outlook.com>
2023-07-21 19:09:46 -0300

    feat(base-converter.js): create decimalToBinary function

--- src/codes/data-structures/stack/stack.js
+++ src/codes/data-structures/stack/stack.js
@@ -89,3 +89,24 @@ stackWeakMap.push(1);
 
 console.log(Object.getOwnPropertyNames(stack));
 console.log(Object.getOwnPropertyNames(stackWeakMap));
+
+
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = '';
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+}
+
+console.log(decimalToBinary(10));
