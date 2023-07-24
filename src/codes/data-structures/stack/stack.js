645f1d6f53645ff2a799abfa0e17508330d1efec
Bruno Pinho <brunopinho@outlook.com>
2023-07-24 15:04:46 -0300

    feat: create baseConventer function

--- src/codes/data-structures/stack/stack.js
+++ src/codes/data-structures/stack/stack.js
@@ -1,4 +1,4 @@
-class Stack {
+export class Stack {
   constructor() {
     this.count = 0;
     this.items = {};
@@ -110,3 +110,32 @@ function decimalToBinary(decNumber) {
 }
 
 console.log(decimalToBinary(10));
+
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let number = decNumber;
+  let rem;
+  let baseString = '';
+
+  if (!(base >= 2 && base <= 36)) {
+    return '';
+  }
+
+  while (number > 0) {
+    rem = Math.floor(number % base);
+    remStack.push(rem);
+    number = Math.floor(number / base);
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter(100345, 2));
+console.log(baseConverter(100345, 8));
+console.log(baseConverter(100345, 16));
+console.log(baseConverter(100345, 36));
